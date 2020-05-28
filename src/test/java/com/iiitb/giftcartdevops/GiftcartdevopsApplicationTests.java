4af6aee6b074439c858570adb1b6d2c66de1e985
Abhishek Acharya <acharya.n.abhi@gmail.com>
2020-05-28 15:29:32 +0530

    Update GiftcartdevopsApplicationTests.java

--- src/test/java/com/iiitb/giftcartdevops/GiftcartdevopsApplicationTests.java
+++ src/test/java/com/iiitb/giftcartdevops/GiftcartdevopsApplicationTests.java
@@ -1,6 +1,7 @@
 package com.iiitb.giftcartdevops;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 @ExtendWith(SpringExtension.class)
 //@SpringBootTest
